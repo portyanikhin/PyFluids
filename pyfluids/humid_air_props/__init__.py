126b6f1f94e919205ef65ed33ffec414c4e47581
Vladimir Portyanikhin <86243191+portyanikhin@users.noreply.github.com>
2022-08-21 13:45:57 +0300

    Revert "Release"
    This reverts commit 11ac46ebad3fd35ab068ec3aaf2c2260d34609a4.

--- pyfluids/humid_air_props/__init__.py
+++
@@ -1,7 +0,0 @@
-#  PyFluids
-#  Copyright (c) 2021 Vladimir Portyanikhin
-
-from .humid_air import *
-from .humid_air_inputs import *
-
-__all__ = humid_air.__all__ + humid_air_inputs.__all__
--- pyfluids/interfaces/__init__.py
+++
@@ -1,7 +0,0 @@
-#  PyFluids
-#  Copyright (c) 2021 Vladimir Portyanikhin
-
-from .fluid_interfaces import *
-from .input_interfaces import *
-
-__all__ = fluid_interfaces.__all__ + input_interfaces.__all__
