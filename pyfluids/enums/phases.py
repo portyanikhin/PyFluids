4fd53bff14267c45e8db83d868e663661e7c25bc
Vladimir Portyanikhin <86243191+portyanikhin@users.noreply.github.com>
2022-08-30 14:28:23 +0300

    Update phases enum

--- pyfluids/enums/phases.py
+++ pyfluids/enums/phases.py
@@ -1,3 +1,5 @@
+import CoolProp
+
 from enum import Enum
 
 __all__ = ["Phases"]
@@ -6,15 +8,15 @@ __all__ = ["Phases"]
 class Phases(Enum):
     """Fluids and mixtures phase states."""
 
-    Liquid = "Liquid"
-    Supercritical = "Supercritical"
-    SupercriticalGas = "Supercritical gas"
-    SupercriticalLiquid = "Supercritical liquid"
-    CriticalPoint = "Critical point"
-    Gas = "Gas"
-    TwoPhase = "Two phase"
-    Unknown = "Unknown"
-    NotImposed = "Not imposed"
+    Liquid = CoolProp.iphase_liquid
+    Supercritical = CoolProp.iphase_supercritical
+    SupercriticalGas = CoolProp.iphase_supercritical_gas
+    SupercriticalLiquid = CoolProp.iphase_supercritical_liquid
+    CriticalPoint = CoolProp.iphase_critical_point
+    Gas = CoolProp.iphase_gas
+    TwoPhase = CoolProp.iphase_twophase
+    Unknown = CoolProp.iphase_unknown
+    NotImposed = CoolProp.iphase_not_imposed
 
     def __repr__(self):
         return self.name
