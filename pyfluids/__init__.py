afe6bf34c8d03a3b6fbf7b2f7c6775affc6643a3
Vladimir Portyanikhin <86243191+portyanikhin@users.noreply.github.com>
2023-05-10 00:00:04 +0300

    Added PyFluidsConfig class

--- pyfluids/__init__.py
+++ pyfluids/__init__.py
@@ -1,6 +1,9 @@
+from .config import *
 from .enums import *
 from .fluids import *
 from .humid_air import *
 from .io import *
 
-__all__ = enums.__all__ + fluids.__all__ + humid_air.__all__ + io.__all__
+__all__ = (
+    config.__all__ + enums.__all__ + fluids.__all__ + humid_air.__all__ + io.__all__
+)
