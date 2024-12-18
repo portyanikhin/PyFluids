ec9f308e6ff05e3895f0ad8db476374bb2a761c1
Vladimir Portyanikhin <86243191+portyanikhin@users.noreply.github.com>
2024-12-18 17:35:52 +0300

    Refactoring

--- pyfluids/humid_air/humid_air.py
+++ pyfluids/humid_air/humid_air.py
@@ -474,16 +474,12 @@ class HumidAir:
             ),
             InputHumidAir.humidity(
                 (
-                    first_specific_mass_flow * first.humidity
-                    + first_specific_mass_flow * first.humidity * second.humidity
-                    + second_specific_mass_flow * second.humidity
-                    + second_specific_mass_flow * first.humidity * second.humidity
+                    first_specific_mass_flow * first.humidity * (1 + second.humidity)
+                    + second_specific_mass_flow * second.humidity * (1 + first.humidity)
                 )
                 / (
-                    first_specific_mass_flow
-                    + first_specific_mass_flow * second.humidity
-                    + second_specific_mass_flow
-                    + second_specific_mass_flow * first.humidity
+                    first_specific_mass_flow * (1 + second.humidity)
+                    + second_specific_mass_flow * (1 + first.humidity)
                 )
             ),
         )
